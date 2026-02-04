5135dd1887de577306244f04c60faf8d061c9c29
Claire Lin <jillian236@gmail.com>
2026-02-04 14:40:21 +0800

    feat: add registration API and enable JPA auditing

--- src/main/java/ecommerce/EcommerceApplication.java
+++ src/main/java/ecommerce/EcommerceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaAuditing // 必須加上這一行，才會自動填寫 createdAt
 public class EcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
