ec0fa87c41e9a88d2c44429c0c3ea666cd9d2b4d
Claire Lin <jillian236@gmail.com>
2026-02-05 17:56:40 +0800

    Update JWT secret config and inject JwtUtils into UserService

--- src/main/java/ecommerce/service/impl/UserServiceImpl.java
+++ src/main/java/ecommerce/service/impl/UserServiceImpl.java
@@ -19,10 +19,12 @@ public class UserServiceImpl implements UserService{
 
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
+	private final JwtUtils jwtUtils;
 	
-	public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+	public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtils jwtUtils) {
 		this.userRepository = userRepository;
 		this.passwordEncoder = passwordEncoder;
+		this.jwtUtils = jwtUtils;
 	}
 	
 	@Override
