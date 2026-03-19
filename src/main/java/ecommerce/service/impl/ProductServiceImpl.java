99549938b71ba7fa184301dae2c2f4b1f4a5d46a
Claire Lin <jillian236@gmail.com>
2026-03-19 18:06:01 +0800

    feat: ProductServiceImpl 補上 patchProduct 方法，支援商品部分更新

--- src/main/java/ecommerce/service/impl/ProductServiceImpl.java
+++ src/main/java/ecommerce/service/impl/ProductServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import ecommerce.dto.CreateProductRequest;
+import ecommerce.dto.PatchProductRequest;
 import ecommerce.dto.UpdateProductRequest;
 import ecommerce.entity.Product;
 import ecommerce.repository.ProductRepository;
@@ -49,9 +50,34 @@ public class ProductServiceImpl implements ProductService{
 		product.setName(request.getName());
 		product.setPrice(request.getPrice());
 		product.setStock(request.getStock());
-		// 若有 imageUrl 欄位，請補上
 		product.setImageUrl(request.getImageUrl());
 		return productRepository.save(product);
+	}
+	
+	@Override
+	@Transactional
+	public Product patchProduct(Long id, PatchProductRequest request) {
+		Product product = getProduct(id);
+		if (request.getName() != null && !request.getName().isEmpty()) {
+			product.setName(request.getName());
+		}
+		if (request.getPrice() != null) {
+			product.setPrice(request.getPrice());
+		}
+		if (request.getStock() != null) {
+			product.setStock(request.getStock());
+		}
+		if (request.getImageUrl() != null && !request.getImageUrl().isEmpty()) {
+			product.setImageUrl(request.getImageUrl());
+		}
+		return productRepository.save(product);
+	}
+	
+	@Override
+	@Transactional
+	public void deleteProduct(Long id) {
+		Product product = getProduct(id);
+		productRepository.delete(product);
 	}
 	
 	
